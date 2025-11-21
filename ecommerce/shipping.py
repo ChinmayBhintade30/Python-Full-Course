400c0076016e3965f5565b3fd8fd53cbfcd445ae
ChinmayBhintade30 <chinmaybhintade2004@gmail.com>
2025-11-22 00:49:34 +0530

    ecommerce package

---
+++ ecommerce/shipping.py
@@ -0,0 +1,7 @@
+#in this module which is also a file we have to calculate the shipping cots of the product
+
+#we can import the entire modoule or we can import the entire classes in it
+
+def calc_shipping():
+    print("calc_shipping!")
+
